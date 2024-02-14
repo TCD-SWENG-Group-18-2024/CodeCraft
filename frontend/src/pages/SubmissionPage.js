be168d8631a28ec19ce83a4590dcd8eab450c284
yzhuangintcd <127947054+yzhuangintcd@users.noreply.github.com>
2024-02-14 12:09:16 +0000

    Change "http://127.0.0.1:8080/llm" to "http://localhost:8080/llm" in S

--- frontend/src/pages/SubmissionPage.js
+++ frontend/src/pages/SubmissionPage.js
@@ -50,7 +50,7 @@ const SubmissionPage = () => {
         console.log(data);
         
         try {
-            const response = await fetch("http://127.0.0.1:8080/llm", 
+            const response = await fetch("http://localhost:8080/llm", 
             {
                 method: "POST",
                 headers: {
