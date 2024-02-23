7ce4937b643a05c847bb24facd22b7c20f917dc7
luciabrown <brownlu@tcd.ie>
2024-02-23 11:34:18 +0000

    First draft of POST request to export LLM response

--- backend/codecraft.py
+++ backend/codecraft.py
@@ -1,14 +1,15 @@
 import requests
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, send_file
 from flask_cors import CORS
 from response import code_generation, code_completion, code_translation, code_analysis, AIModel
+import os
 
 app = Flask(__name__)
 CORS(app)
 
 UPLOAD_FOLDER = 'uploads'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-ALLOWED_EXTENSIONS = {'txt', 'py', 'c', 'cpp', 'java', 'cs', 'S', 'asm' 'js', 'html','css','rb','php','kt','R','pl'}
+ALLOWED_EXTENSIONS = {'txt', 'py', 'c', 'cpp', 'java', 'cs', 'S', 'js', 'html','css','rb','php','kt','R','pl'}
 MAX_FILE_SIZE_BYTES = 10 * 1024  #10KB
 
 @app.route('/')
@@ -49,7 +50,7 @@ def llm_file_request():
 
     file_data = request.files['file']
 
-    # Check if file is too large
+    # Check if file is too large    
     if file_data.content_length > MAX_FILE_SIZE_BYTES:
         return jsonify({'error': 'File size exceeds the limit of 10KB'}), 400
 
@@ -88,6 +89,58 @@ def process_data(user_input, use_case, ai_model, input_language, output_language
         result = {"error": "Invalid use case"}
 
     return result
+
+#POST method to occur when user chooses to export on the frontend
+@app.route('/export', methods=['POST'])
+def export_endpoint():
+    # Extract data from the request
+    data = request.get_json()           
+    llm_response = data.get('llm_response')     #llm response
+    filename = data.get('filename', 'response') #the filename user wants, if none, default to response
+    output_language = data.get('output_language') #take in output_language
+
+    #if there is an output language given, make the file extension correspond
+    if output_language != '':                
+        if output_language.lower() == 'python':
+            filename += '.py'
+        elif output_language.lower() =='c':
+            filename += '.c'
+        elif output_language.lower() =='c++':
+            filename += '.cpp'
+        elif output_language.lower() =='java':
+            filename += '.java'
+        elif output_language.lower() =='c#':
+            filename += '.cs'
+        elif output_language.lower() =='assembly':
+            filename += '.S'
+        elif output_language.lower() =='javascript':
+            filename += '.js'
+        elif output_language.lower() =='html':
+            filename += '.html'
+        elif output_language.lower() =='css':
+            filename += '.css'
+        elif output_language.lower() =='ruby':
+            filename += '.rb'
+        elif output_language.lower() =='php':
+            filename += '.php'
+        elif output_language.lower() =='kotlin':
+            filename += '.kt'
+        elif output_language.lower() =='r':
+            filename += '.R'
+        elif output_language.lower() =='perl':
+            filename += '.pl'
+        else:
+            filename += '.txt' #default to .txt
+    #if no output_language default to .txt
+    else:
+        filename += '.txt'
+
+
+    # Export LLM response to file
+    with open(filename, 'w') as f:
+        f.write(llm_response)
+    # Return the exported file to the client as an attachment
+    return send_file(filename, as_attachment=True)
 
 if __name__ == "__main__":
     app.run(debug=True, port=8080)
