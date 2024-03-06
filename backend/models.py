aa3944daaa83f59d1faa3436637e45441146d4b5
abkhan04 <abkhan@tcd.ie>
2024-03-06 00:05:16 +0000

    Moved User from signin.py to models.py

---
+++ backend/models.py
@@ -0,0 +1,15 @@
+from flask_login import UserMixin
+from flask_sqlalchemy import SQLAlchemy
+from uuid import uuid4
+
+db = SQLAlchemy()
+
+def get_uuid():
+    return uuid4().hex
+
+
+class User(db.Model):
+    __tablename__ = "users"
+    id = db.Column(db.String(32), primary_key=True, unique=True, default=get_uuid)
+    username = db.Column(db.String(20), nullable=False, unique=True)
+    password = db.Column(db.Text, nullable=False)
