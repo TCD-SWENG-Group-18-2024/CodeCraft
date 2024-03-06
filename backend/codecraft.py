0c63be8a5270e24fb1760232d7d0b98d57432918
abkhan04 <abkhan@tcd.ie>
2024-03-06 00:07:54 +0000

    Created login and register functions

--- backend/codecraft.py
+++ backend/codecraft.py
@@ -1,23 +1,36 @@
-import requests
-from flask import Flask, request, jsonify, send_file
+from config import ApplicationConfig
+from flask import Flask, request, jsonify, send_file, abort
 from flask_cors import CORS
+from flask_bcrypt import Bcrypt
+from models import db, User
 from response import code_generation, code_completion, code_translation, code_analysis, AIModel
-import os
 
 app = Flask(__name__)
 CORS(app)
 
 UPLOAD_FOLDER = 'uploads'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-ALLOWED_EXTENSIONS = {'txt', 'py', 'c', 'cpp', 'java', 'cs', 'S', 'js', 'html','css','rb','php','kt','R','pl'}
-MAX_FILE_SIZE_BYTES = 10 * 1024  #10KB
+ALLOWED_EXTENSIONS = {'txt', 'py', 'c', 'cpp', 'java', 'cs',
+                      'S', 'js', 'html', 'css', 'rb', 'php', 'kt', 'R', 'pl'}
+MAX_FILE_SIZE_BYTES = 10 * 1024  # 10KB
+
+# Initialise database and configure
+app.config.from_object(ApplicationConfig)
+bcrypt = Bcrypt(app)
+db.init_app(app)
+
+with app.app_context():
+    db.create_all()
+
 
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
+
 @app.route('/')
 def homepage():
     return {"message": "Hello SwEng Project Group 18"}
+
 
 @app.route('/llm/text', methods=['POST'])
 def llm_text_request():
@@ -34,13 +47,15 @@ def llm_text_request():
         return jsonify({'error': 'No user input provided'}), 400
 
     # Call the appropriate function based on use_case and ai_model
-    result = process_data(user_input, use_case, ai_model, input_language, output_language)
+    result = process_data(user_input, use_case, ai_model,
+                          input_language, output_language)
 
     return jsonify(result)
 
+
 @app.route('/llm/file', methods=['POST'])
 def llm_file_request():
-    
+
     # Extract parameters from JSON payload
     use_case = request.form.get('use_case')
     ai_model = request.form.get('ai_model')
@@ -52,13 +67,12 @@ def llm_file_request():
         return jsonify({'error': 'No file uploaded'}), 400
 
     file_data = request.files['file']
-    
+
     # Check if the file has an allowed extension
     if not allowed_file(file_data.filename):
         return jsonify({'error': 'Invalid file extension'}), 400
 
-
-    # Check if file is too large    
+    # Check if file is too large
     if file_data.content_length > MAX_FILE_SIZE_BYTES:
         return jsonify({'error': 'File size exceeds the limit of 10KB'}), 400
 
@@ -73,9 +87,11 @@ def llm_file_request():
         return jsonify({'error': 'Failed to decode file content as UTF-8'}), 400
 
     # Call the appropriate function based on use_case and ai_model
-    result = process_data(user_input, use_case, ai_model, input_language, output_language)
+    result = process_data(user_input, use_case, ai_model,
+                          input_language, output_language)
 
     return jsonify(result)
+
 
 def process_data(user_input, use_case, ai_model, input_language, output_language):
     if use_case is not None:
@@ -88,59 +104,62 @@ def process_data(user_input, use_case, ai_model, input_language, output_language
     elif use_case == 'code_completion':
         result = code_completion(user_input, ai_model, input_language)
     elif use_case == 'code_translation':
-        result = code_translation(input_language, output_language, user_input, ai_model)
+        result = code_translation(
+            input_language, output_language, user_input, ai_model)
     elif use_case == '':
         # general model for no specified operation
         result = AIModel(user_input, ai_model)
     else:
         result = {"error": "Invalid use case"}
-    
+
     # TODO: Add more conditions for other AI models
     # TODO: Can add more conditions for other use cases
 
     return result
 
+
 # POST method to occur when user chooses to export on the frontend
 @app.route('/export', methods=['POST'])
 def export_endpoint():
     # Extract data from the request
-    data = request.get_json()           
+    data = request.get_json()
     llm_response = data.get('llm_response')         # llm response
-    filename = data.get('filename', 'response')     # the filename user wants, if none, default to response
+    # the filename user wants, if none, default to response
+    filename = data.get('filename', 'response')
     output_language = data.get('output_language')   # take in output_language
 
     # if there is an output language given, make the file extension correspond
-    if output_language != '':                
+    if output_language != '':
         if output_language.lower() == 'python':
             filename += '.py'
-        elif output_language.lower() =='c':
+        elif output_language.lower() == 'c':
             filename += '.c'
-        elif output_language.lower() =='c++':
+        elif output_language.lower() == 'c++':
             filename += '.cpp'
-        elif output_language.lower() =='java':
+        elif output_language.lower() == 'java':
             filename += '.java'
-        elif output_language.lower() =='c#':
+        elif output_language.lower() == 'c#':
             filename += '.cs'
-        elif output_language.lower() =='assembly':
+        elif output_language.lower() == 'assembly':
             filename += '.S'
-        elif output_language.lower() =='javascript':
+        elif output_language.lower() == 'javascript':
             filename += '.js'
-        elif output_language.lower() =='html':
+        elif output_language.lower() == 'html':
             filename += '.html'
-        elif output_language.lower() =='css':
+        elif output_language.lower() == 'css':
             filename += '.css'
-        elif output_language.lower() =='ruby':
+        elif output_language.lower() == 'ruby':
             filename += '.rb'
-        elif output_language.lower() =='php':
+        elif output_language.lower() == 'php':
             filename += '.php'
-        elif output_language.lower() =='kotlin':
+        elif output_language.lower() == 'kotlin':
             filename += '.kt'
-        elif output_language.lower() =='r':
+        elif output_language.lower() == 'r':
             filename += '.R'
-        elif output_language.lower() =='perl':
+        elif output_language.lower() == 'perl':
             filename += '.pl'
         else:
-            filename += '.txt' #default to .txt
+            filename += '.txt'  # default to .txt
     # if no output_language default to .txt
     else:
         filename += '.txt'
@@ -148,9 +167,54 @@ def export_endpoint():
     # Export LLM response to file
     with open(filename, 'w') as f:
         f.write(llm_response)
-    
+
     # Return the exported file to the client as an attachment
     return send_file(filename, as_attachment=True)
+
+
+@app.route('/register', methods=['POST'])
+def register_user():
+    username = request.json['username']
+    password = request.json['password']
+
+    if username == '':
+        return jsonify({"error": "No username provided"}), 400
+
+    user_exists = User.query.filter_by(username=username).first() is not None
+
+    if user_exists:
+        return jsonify({"error": "User already exists"}), 409
+
+    hashed_password = bcrypt.generate_password_hash(password)
+    new_user = User(username=username, password=hashed_password)
+
+    db.session.add(new_user)
+    db.session.commit()
+
+    return jsonify({
+        "id": new_user.id,
+        "username": new_user.username
+    })
+
+
+@app.route('/login', methods=['POST'])
+def login_user():
+    username = request.json['username']
+    password = request.json['password']
+
+    user = User.query.filter_by(username=username).first()
+
+    if user is None:
+        return jsonify({"error": "User does not exist"}), 401
+
+    if not bcrypt.check_password_hash(user.password, password):
+        return jsonify({"error": "Incorrect password"}), 401
+
+    return jsonify({
+        "id": user.id,
+        "username": user.username
+    })
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080)
