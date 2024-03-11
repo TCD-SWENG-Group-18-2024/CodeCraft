796f1152df7535c71f769fbdc59ae3b0af0f9c46
archbolp <114676027+archbolp@users.noreply.github.com>
2024-03-11 17:08:06 +0000

    Update Sidebar.js

--- frontend/src/components/Sidebar.js
+++ frontend/src/components/Sidebar.js
@@ -7,6 +7,8 @@ import '../styles/Sidebar.css';
 const Sidebar = ({ isOpen, toggleSidebar }) => {
   const location = useLocation();
   const isHomePage = location.pathname === "/";
+  const isExportPage = location.pathname === "/ExportPage";
+  const isSubmissionPage = location.pathname === "/SubmissionPage";
 
   return (
     <div className={`sidebar ${isOpen ? 'sidebar--open' : ''}`}>
@@ -21,14 +23,34 @@ const Sidebar = ({ isOpen, toggleSidebar }) => {
       </div>
       <nav className="sidebar__nav">
         {isHomePage && (
-          <Link to="/SubmissionPage">
+          <>
+          <Link to="/SubmissionPage" className="sidebar_link">
             <FontAwesomeIcon icon={faChartBar} /> Submission Page
           </Link>
+          <Link to="/ExportPage" className="sidebar_link">
+            <FontAwesomeIcon icon={faChartBar} />  Export
+          </Link>
+          </>
         )}
-        {!isHomePage && (
+        {isExportPage && (
+          <>
           <Link to="/" className="sidebar_link">
             <FontAwesomeIcon icon={faDashboard} /> Dashboard
           </Link>
+          <Link to="/SubmissionPage" className="sidebar_link">
+            <FontAwesomeIcon icon={faChartBar} />  Submission Page
+          </Link>
+          </>
+        )}
+        {isSubmissionPage && (
+          <>
+          <Link to="/" className="sidebar_link">
+            <FontAwesomeIcon icon={faDashboard} /> Dashboard
+          </Link>
+          <Link to="/ExportPage" className="sidebar_link">
+            <FontAwesomeIcon icon={faChartBar} />  Export Page
+          </Link>
+          </>
         )}
       </nav>
     </div>
