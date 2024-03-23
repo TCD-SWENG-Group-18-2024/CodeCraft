6d6426e1bf6f3d390ccc39ca82d8fee90fb13681
mariamquesada <quesadam@tcd.ie>
2024-03-23 20:33:22 +0000

    Adding logo to header

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,7 +1,7 @@
 import React , {useContext} from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import {AuthContext} from "../components/AuthContext"
-import HeaderImage from '../assets/IBM_white.PNG';
+import HeaderImage from '../assets/logo.png';
 import '../styles/Header.css';
 
 const Header = () => {
@@ -19,23 +19,21 @@ const Header = () => {
             <Link to="/account">Account</Link>
             {/* The IBM logo will only show when not on the home page */}
             {!isHomePage && (
-              <a href="https://www.ibm.com/us-en" target="_blank" rel="noopener noreferrer">
                 <img src={HeaderImage} alt="IBM Logo" className="header-image" />
-              </a>
             )}
           </div>
         ) : (
           // Links to show when the user is not logged in
           <div>
-            <Link to="/">Home</Link>
+            
             {!isSignUpPage && (
               <Link to="/loginSignUp" className="App-sign-up">Sign up</Link>
             )}
             {/* The IBM logo will only show when not on the home page */}
             {!isHomePage && (
-              <a href="https://www.ibm.com/us-en" target="_blank" rel="noopener noreferrer">
+                <Link to="/">
                 <img src={HeaderImage} alt="IBM Logo" className="header-image"/>
-              </a>
+                </Link>
             )}
           </div>
         )}
