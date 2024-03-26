c90a90d260427620ab23130ee76a0232876975ef
FinnStrike <murphf15@tcd.ie>
2024-03-26 09:48:03 +0000

    Removed Header BG and Added Home Logo

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import IBM_White from "../assets/IBM_white.PNG";
 import Sidebar from '../components/Sidebar';
+import HeaderImage from '../assets/logo.png';
 import '../styles/Home.css';
 
 function Home() {
@@ -25,10 +26,10 @@ function Home() {
         <>
             <div className="App">
                 <Sidebar isOpen={isSidebarOpen} toggleSidebar={toggleSidebar} />
-                <a href='https://www.ibm.com/us-en' target='_blank' rel="noreferrer">
+                <a href='https://www.ibm.com/us-en' target='_blank' rel="noreferrer" style={{display: "flex", flexDirection: "column", alignItems: "center"}}>
                     <img src={IBM_White} alt="IBM Logo" className={`logo ${isLogoLoaded ? 'loaded' : ''}`} />
+                    <img src={HeaderImage} alt="CodeCraft Logo" className="codecraft-logo"/>
                 </a>
-                <h1 className="ProjectName">CodeCraft</h1>
                 <h3 className="welcome-message">Brought to you by Group 18</h3>
                 <Link to="/SubmissionPage" className="tab-link">
                     <button className="StartAnalysisButton">
