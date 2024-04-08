493146780d757eba2599acdfbf88f3d8c320070c
Fionn Murphy <115025266+FinnStrike@users.noreply.github.com>
2024-04-08 13:16:24 +0100

    Latest Frontend App.test.js

--- frontend/src/frontend tests/App.test.js
+++ frontend/src/frontend tests/App.test.js
@@ -6,6 +6,13 @@ jest.mock('react-syntax-highlighter/dist/esm/styles/prism', () => ({
   nord: jest.fn(),
 }));
 
+jest.mock('react-markdown', () => {
+  return {
+    __esModule: true,
+    default: () => <div>Mocked ReactMarkdown component</div>,
+  };
+});
+
 test('renders App component without errors', () => {
   render(<App />);
 });
